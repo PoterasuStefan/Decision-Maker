5a18174892cadb666baa49eb4a24fa84bbfba5af
stefan poterasu <spoterasu@gmail.com>
2023-05-01 12:58:20 +0300

    Finished base app

--- Decision Maker/Form1.Designer.cs
+++ Decision Maker/Form1.Designer.cs
@@ -29,12 +29,139 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.Input = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Result = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Mode = new System.Windows.Forms.Button();
+            this.History = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Input
+            // 
+            this.Input.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Input.Location = new System.Drawing.Point(598, 45);
+            this.Input.Multiline = true;
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(190, 344);
+            this.Input.TabIndex = 0;
+            this.Input.Text = "Option one...\r\nOption two...\r\n";
+            this.Input.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(330, 196);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Result
+            // 
+            this.Result.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Result.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Result.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Result.Location = new System.Drawing.Point(598, 421);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(190, 20);
+            this.Result.TabIndex = 3;
+            this.Result.Text = "Result will appear here...";
+            this.Result.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(598, 395);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Mode
+            // 
+            this.Mode.Location = new System.Drawing.Point(697, 395);
+            this.Mode.Name = "Mode";
+            this.Mode.Size = new System.Drawing.Size(91, 23);
+            this.Mode.TabIndex = 5;
+            this.Mode.Text = "Mode: Keep";
+            this.Mode.UseVisualStyleBackColor = true;
+            this.Mode.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // History
+            // 
+            this.History.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.History.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.History.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.History.Location = new System.Drawing.Point(12, 45);
+            this.History.Name = "History";
+            this.History.Size = new System.Drawing.Size(178, 344);
+            this.History.TabIndex = 6;
+            this.History.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "History:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(12, 392);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(178, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.History);
+            this.Controls.Add(this.Mode);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Input);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label Result;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Mode;
+        private System.Windows.Forms.Label History;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Decision Maker/Form1.cs
+++ Decision Maker/Form1.cs
@@ -12,9 +12,68 @@ namespace Decision_Maker
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
+        int Winner;
+
         public Form1()
         {
             InitializeComponent();
+
+            
+        }
+
+
+        int Mode_nr = 1;
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var lines = Input.Lines.Where(line => !String.IsNullOrWhiteSpace(line)).Count();
+            Winner = random.Next(0, lines);
+            
+            if (Mode_nr==1)
+            {
+                Result.Text = Input.Lines[Winner];
+            }
+            else if (lines>=1)
+            {
+                Result.Text = Input.Lines[Winner];
+                List<string> linesList = new List<string>(Input.Lines);
+                linesList.RemoveAt(Winner);
+                Input.Lines = linesList.ToArray();
+                
+            }
+            else
+            {
+                MessageBox.Show("No options left");
+            }
+            History.Text = Result.Text + "\n" + History.Text;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Input.Text = null;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(Mode.Text=="Mode: Keep")
+            {
+                Mode.Text = "Mode: Cut";
+                Mode_nr = 0;
+            }
+            else
+            {
+                Mode.Text = "Mode: Keep";
+                Mode_nr = 1;
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            History.Text = null;
         }
     }
 }
